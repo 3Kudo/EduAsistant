684466ff6fee01979fba0d8da24637ef1c7fb59b
3Kudo <116746067+3Kudo@users.noreply.github.com>
2026-05-18 13:52:27 +0200

    creating login/registation web

---
+++ EduAsistant/EduAsistant/Areas/Identity/Data/EduAsistantContext.cs
@@ -0,0 +1,22 @@
+﻿using EduAsistant.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduAsistant.Data;
+
+public class EduAsistantContext : IdentityDbContext<Student>
+{
+    public EduAsistantContext(DbContextOptions<EduAsistantContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
